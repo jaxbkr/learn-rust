78074d028064c86e1092076e6709b0d7e85727ee
Jackson Baker <jackson.baker7562@gmail.com>
2026-05-29 00:05:50 -0500

    rm target dirs:

--- hello-world/hello-cargo/target/debug/deps/hello_cargo-65c0033a86f16e42.d
+++
@@ -1,5 +0,0 @@
-/home/jax/Github/learn-rust/hello-world/hello-cargo/target/debug/deps/hello_cargo-65c0033a86f16e42.d: src/main.rs
-
-/home/jax/Github/learn-rust/hello-world/hello-cargo/target/debug/deps/hello_cargo-65c0033a86f16e42: src/main.rs
-
-src/main.rs:
--- hello-world/hello-cargo/target/debug/hello-cargo.d
+++
@@ -1,1 +0,0 @@
-/home/jax/Github/learn-rust/hello-world/hello-cargo/target/debug/hello-cargo: /home/jax/Github/learn-rust/hello-world/hello-cargo/src/main.rs
