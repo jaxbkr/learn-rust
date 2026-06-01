19e8c6e736d72961e28fb56fc5bc3609a8fbf01b
Jackson Baker <jackson.baker7562@gmail.com>
2026-05-31 23:44:43 -0500

    structs

---
+++ learn-variables/structs/src/main.rs
@@ -0,0 +1,88 @@
+#![allow(unused)]
+
+use std::sync::MutexGuard;
+
+// Struct
+#[derive(Debug)]
+struct Point {
+    x: f32,
+    y: f32,
+}
+
+// Methods
+
+impl Point {
+    // associated function == static methods
+    fn zero() -> Self {
+        Self { x: 0.0, y: 0.0 }
+    }
+
+    // Methods == work on instances of points
+
+    fn move_to(&mut self, x: f32, y: f32) {
+        self.x = x;
+        self.y = y;
+    }
+
+    fn dist_from_origin(&self) -> f32 {
+        return (self.x * self.x + self.y * self.y).sqrt();
+    }
+}
+
+struct Point3D(f32, f32, f32);
+
+struct Empty;
+
+// nested
+#[derive(Debug)]
+struct Circle {
+    center: Point,
+    radius: u32,
+}
+
+fn main() {
+    let p = Point { x: 1.0, y: 2.0 };
+    println!("point.x = {}, point.y = {}", p.x, p.y);
+
+    let p3D = Point3D(1.0, 2.0, 3.0);
+    println!(
+        "point3D.x = {}, point3D.y = {}, point3D.z = {}",
+        p3D.0, p3D.1, p3D.2
+    );
+
+    let empty = Empty;
+
+    let circle = Circle {
+        center: Point { x: 1.0, y: 2.0 },
+        radius: 1,
+    };
+    println!("{:?}", circle);
+
+    // shortcuts
+
+    let x = 1.0;
+    let y = 1.0;
+
+    let p = Point { x, y };
+
+    // copy fields
+
+    let p0 = Point { x: 1.0, y: 1.0 };
+    let p1 = Point { x: 2.0, ..p0 };
+
+    // update a struct
+
+    let mut p = Point { x: 0.0, y: 0.0 };
+    p.x += 1.0;
+    p.y += 2.0;
+    println!("{:?}", p);
+
+    // Using Methods
+
+    let mut p = Point::zero();
+    println!("{:?}", p);
+    p.move_to(3.0, 5.0);
+    println!("{:?}", p);
+    let d = p.dist_from_origin();
+    println!("distance from origin = {:?}", d)
+}
